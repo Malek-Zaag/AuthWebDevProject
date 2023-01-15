60618a77ebd02d12a98161d4b5f152072d64d1fd
malek <zaag.malek1@gmail.com>
2023-01-15 11:53:32 +0100

    adding user model

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { UserModel } from './auth/model/user.model';
     host: 'localhost',
     port: 3306,
     username: "root",
-    password: "root",
+    password: "",
     database: "auth-project-web",
     entities: [UserModel],
     synchronize: true,
--- src/auth/model/user.model.ts
+++ src/auth/model/user.model.ts
@@ -1,13 +1,26 @@
 /* eslint-disable prettier/prettier */
-import { Entity } from 'typeorm';
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity('users')
 export class UserModel {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+  @Column({ unique: true })
   username: string;
+  @Column({
+    length: 8,
+  })
   password: string;
+  @Column({
+    unique: true
+  })
   email: string;
-  movies: string[];
+  @Column()
+  movies: string;
+  @CreateDateColumn()
   createdAt: Date;
+  @UpdateDateColumn()
   updatedAt: Date;
+  @DeleteDateColumn()
+  deletedAt: Date;
 }
